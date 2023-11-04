8ca12df0ba7800fe886f81f8a1c638073c28334b
Frustman <cmne3602@gmail.com>
2023-11-04 22:40:40 +0900

    Feat : Character, Monsters

---
+++ Assets/Scripts/Monster/AnimalsController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalsController : MonoBehaviour
+{
+    public Animator animator;
+
+    public float speed;
+    public Transform target;
+
+    public NavMeshAgent navMeshAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+        animator.Play("Run");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+        if (navMeshAgent.velocity.magnitude > 0f)
+        {
+            animator.Play("Run");
+        }
+        else
+        {
+            animator.Play("Idle_A");
+        }
+    }
+}
